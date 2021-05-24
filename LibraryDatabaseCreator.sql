f014819fbb1082ff74e5409775d2b31061d8783c
RazvanNazare <58268118+RazvanNazare@users.noreply.github.com>
2021-05-24 14:20:16 +0300

    Added and deleted some whitespaces

--- LibraryDatabaseCreator.sql
+++ LibraryDatabaseCreator.sql
@@ -29,14 +29,14 @@ CREATE TABLE Books
 	DiscountPercentage INT DEFAULT 0,
 	CHECK (DiscountPercentage >= 0 AND DiscountPercentage <= 100),
 
-	CONSTRAINT FK_AuthorIDAuthors FOREIGN KEY (AuthorID)
-    REFERENCES Authors(AuthorID),
+CONSTRAINT FK_AuthorIDAuthors FOREIGN KEY (AuthorID)
+REFERENCES Authors(AuthorID),
 
-	CONSTRAINT FK_PublisherIDPublishers FOREIGN KEY (PublisherID)
-    REFERENCES Publishers(PublisherID),
+CONSTRAINT FK_PublisherIDPublishers FOREIGN KEY (PublisherID)
+REFERENCES Publishers(PublisherID),
 
-	CONSTRAINT FK_CategoryIDCategoris FOREIGN KEY (CategoryID)
-    REFERENCES Categories(CategoryID),
+CONSTRAINT FK_CategoryIDCategoris FOREIGN KEY (CategoryID)
+REFERENCES Categories(CategoryID),
 );
 
 ALTER TABLE Books ADD CONSTRAINT uq_Books UNIQUE(BookName, AuthorID)
@@ -53,38 +53,38 @@ CREATE TABLE LibraryBooks
 	LibraryID INT NOT NULL,
 	BookID INT NOT NULL,
 
-	CONSTRAINT LibraryID_BookID_PK PRIMARY KEY (LibraryID, BookID),
+CONSTRAINT LibraryID_BookID_PK PRIMARY KEY (LibraryID, BookID),
 
-	CONSTRAINT FK_LibraryBooks FOREIGN KEY (BookID)
-	REFERENCES Books(BookID),
+CONSTRAINT FK_LibraryBooks FOREIGN KEY (BookID)
+REFERENCES Books(BookID),
 
-	CONSTRAINT FK_BooksLibrary FOREIGN KEY (LibraryID)
-	REFERENCES Libraries(LibraryID)
+CONSTRAINT FK_BooksLibrary FOREIGN KEY (LibraryID)
+REFERENCES Libraries(LibraryID)
 );
 
 CREATE TABLE Departaments
 (
-DepartamentID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-DepartamentName VARCHAR(50),
-LibraryID INT,
-Location VARCHAR(50),
+	DepartamentID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	DepartamentName VARCHAR(50),
+	LibraryID INT,
+	Location VARCHAR(50),
 );
 
 CREATE TABLE Employees
 (
-EmployeeID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-WorkingLibraryID INT NOT NULL,
-FirstName VARCHAR(50) NOT NULL,
-MiddleName VARCHAR(50),
-LastName VARCHAR(50),
-WorkingDepartamentID INT,
-PhoneNumber VARCHAR(15),
-HireDate DATE,
-Job VARCHAR(50),
-Sex VARCHAR(10),
-BirthDate DATE,
-Salary DECIMAL(9,2),
-Bonus DECIMAL(9,2),
+	EmployeeID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	WorkingLibraryID INT NOT NULL,
+	FirstName VARCHAR(50) NOT NULL,
+	MiddleName VARCHAR(50),
+	LastName VARCHAR(50),
+	WorkingDepartamentID INT,
+	PhoneNumber VARCHAR(15),
+	HireDate DATE,
+	Job VARCHAR(50),
+	Sex VARCHAR(10),
+	BirthDate DATE,
+	Salary DECIMAL(9,2),
+	Bonus DECIMAL(9,2),
 
 CONSTRAINT FK_WorkingLibraryID_Library FOREIGN KEY(WorkingLibraryID)
 REFERENCES Libraries(LibraryID)
@@ -92,8 +92,8 @@ REFERENCES Libraries(LibraryID)
 
 CREATE TABLE DepartamentsEmployees
 (
-DepartamentID INT NOT NULL,
-EmployeeID INT NOT NULL,
+	DepartamentID INT NOT NULL,
+	EmployeeID INT NOT NULL,
 
 CONSTRAINT FK_Departaments_Employees FOREIGN KEY (DepartamentID)
 REFERENCES Departaments(DepartamentID),
@@ -104,9 +104,9 @@ REFERENCES Employees(EmployeeID),
 
 CREATE TABLE SoldBooks
 (
-TransactionID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
-BookID INT NOT NULL,
-DateSold DATETIME DEFAULT GETDATE(),
+	TransactionID INT NOT NULL IDENTITY(1,1) PRIMARY KEY,
+	BookID INT NOT NULL,
+	DateSold DATETIME DEFAULT GETDATE(),
 
 CONSTRAINT FK_SoldBookIDBookID FOREIGN KEY (BookID)
 REFERENCES Books (BookID)
@@ -114,9 +114,9 @@ REFERENCES Books (BookID)
 
 CREATE TABLE RefundedBooks
 (
-TransactionID INT NOT NULL,
-BookID INT NOT NULL,
-DateRefund DATETIME DEFAULT GETDATE(),
+	TransactionID INT NOT NULL,
+	BookID INT NOT NULL,
+	DateRefund DATETIME DEFAULT GETDATE(),
 
 CONSTRAINT FK_RefundedBookIDSoldBookID FOREIGN KEY (TransactionID)
 REFERENCES SoldBooks(TransactionID),
@@ -150,8 +150,8 @@ CREATE TABLE LoyalCustomers
 	Email VARCHAR(100),
 	BookIDs INT,
 
-	CONSTRAINT FK_BookIDBookID FOREIGN KEY (BookIDs)
-	REFERENCES Books(BookID)
+CONSTRAINT FK_BookIDBookID FOREIGN KEY (BookIDs)
+REFERENCES Books(BookID)
 );
 
 
